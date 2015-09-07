cc545651fb9759a69b87f433914f152522ddf747
Michael A. Sinclair <squattingmonk@gmail.com>
2015-09-07 00:20:21 -0500

    Fixed install script

--- install.sh
+++ install.sh
@@ -6,13 +6,16 @@ BASEDIR=$(pwd)
 ln -s ${BASEDIR}/git/gitconfig ~/.gitconfig
 
 # bin
-ln -s ${BASEDIR}/bin/ ~/.bin/
+ln -s ${BASEDIR}/bin ~/.bin
 
 # bspwm
 ln -s ${BASEDIR}/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 
 # sxhkd
 ln -s ${BASEDIR}/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+
+# vim
+ln -s ${BASEDIR}/vim/vimrc ~/.vimrc
 
 # X11
 ln -s ${BASEDIR}/X11/xinitrc ~/.xinitrc
