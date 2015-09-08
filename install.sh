a0f62827e3bbcac21ee6df8cb9d7bc8e3c988501
Michael A. Sinclair <squattingmonk@gmail.com>
2015-09-08 00:50:20 -0500

    Added jasonwryan's colors.

--- install.sh
+++ install.sh
@@ -9,12 +9,15 @@ ln -s ${BASEDIR}/git/gitconfig ~/.gitconfig
 ln -s ${BASEDIR}/bin ~/.bin
 
 # bspwm
+mkdir -pv ~/.config/bspwm
 ln -s ${BASEDIR}/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 
 # sxhkd
+mkdir -pv ~/.config/sxhkd
 ln -s ${BASEDIR}/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
 # vim
+mkdir -pv ~/.vim
 ln -s ${BASEDIR}/vim/vimrc ~/.vimrc
 
 # X11
