ad0edc1cf93d395feba75f104f73809597ded037
Michael A. Sinclair <squattingmonk@gmail.com>
2015-09-08 01:19:23 -0500

    figuring out install script deployment

--- install.sh
+++ install.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-
+LINKOPTIONS="sf"
 BASEDIR=$(pwd)
 
 # git
-ln -s ${BASEDIR}/git/gitconfig ~/.gitconfig
+ln -sf ${BASEDIR}/git/gitconfig ~/.gitconfig
 
 # bin
-ln -s ${BASEDIR}/bin ~/.bin
+#ln -s ${BASEDIR}/bin ~/.bin
 
 # bspwm
 mkdir -pv ~/.config/bspwm
-ln -s ${BASEDIR}/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+ln -sf ${BASEDIR}/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 
 # sxhkd
 mkdir -pv ~/.config/sxhkd
-ln -s ${BASEDIR}/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+ln -sf ${BASEDIR}/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
 # vim
-mkdir -pv ~/.vim
-ln -s ${BASEDIR}/vim/vimrc ~/.vimrc
+ln -sf ${BASEDIR}/vim/vimrc ~/.vimrc
+
+mkdir -pv ~/.vim/colors
+for file in ${BASEDIR}/vim/colors/{.,}*; do
+    ln -${LINKOPTIONS} ${file} ~/.vim/colors/$(basename ${file})
+done
 
 # X11
-ln -s ${BASEDIR}/X11/xinitrc ~/.xinitrc
-ln -s ${BASEDIR}/X11/Xresources ~/.Xresources
+ln -sf ${BASEDIR}/X11/xinitrc ~/.xinitrc
+ln -sf ${BASEDIR}/X11/Xresources ~/.Xresources
