2e5949aa3cf0225ac17fc0ff6d46a0e18f73e676
Michael A. Sinclair <squattingmonk@gmail.com>
2016-01-24 17:11:54 -0600

    Auto-start X on login.

--- fish/config.fish
+++ fish/config.fish
@@ -13,3 +13,10 @@ set -U fish_user_paths $HOME/.bin $fish_user_paths
 set -x BROWSER     chromium
 set -x EDITOR      vim
 set -x SXHKD_SHELL /bin/sh
+
+# Start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+        exec startx -- -keeptty
+    end
+end
