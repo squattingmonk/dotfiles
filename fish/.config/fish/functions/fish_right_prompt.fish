87434b0fac060c7accafcafcff362579358d544e
Michael A. Sinclair <squattingmonk@gmail.com>
2017-11-08 16:12:36 -0600

    Fix right prompt formatting.

--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -1,9 +1,11 @@
 set __fish_git_prompt_showcolorhints 'yes'
 set __fish_git_prompt_show_informative_status 'yes'
+set __fish_git_prompt_showuntrackedfiles ''
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate ' ⚡'
-set __fish_git_prompt_char_stagedstate ' → '
+set __fish_git_prompt_char_dirtystate ' ✚ '
+set __fish_git_prompt_char_stagedstate ' ● '
+set __fish_git_prompt_char_conflicted_state ' ✖ '
 set __fish_git_prompt_char_stashstate ' ↩ '
 set __fish_git_prompt_char_untrackedfiles ' … '
 set __fish_git_prompt_char_upstream_ahead ' ↑ '
@@ -11,5 +13,5 @@ set __fish_git_prompt_char_upstream_behind ' ↓ '
 set __fish_git_prompt_char_stateseparator ' |'
 
 function fish_right_prompt --description 'Returns a git statusline for display in the prompt.'
-    __fish_git_prompt
+    echo -n -s (__fish_git_prompt)
 end
