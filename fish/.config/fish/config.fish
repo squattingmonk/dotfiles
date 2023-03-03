bf773e9da5046dc6c8a381159e8f700cab6d86f8
Michael A. Sinclair <squattingmonk@gmail.com>
2023-03-03 12:14:17 -0600

    scripts: add shortcuts for nwmain and nwtoolset

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -57,6 +57,7 @@ set -x XRESOURCES $XDG_CONFIG_HOME/xorg/xresources
 # Steam helpers
 set -x STEAM_COMPAT_DATA_PATH $XDG_DATA_HOME/proton
 set -x STEAM_PATH $XDG_DATA_HOME/Steam/steamapps/common
+set -x NWN_ROOT "$STEAM_PATH/Neverwinter Nights"
 
 # Aliases for programs that dont's respect XDG
 if not functions -q tmux
