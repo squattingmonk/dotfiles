eb8fc65f39b9131a4f74bc19a9fa14e5fd5b5b74
Michael A. Sinclair <squattingmonk@gmail.com>
2023-03-30 13:16:57 -0500

    neovim: customize tab-completion

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,54 +1,93 @@
 return {
-  -- Use <tab> for completion and snippets (supertab)
-  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
     "L3MON4D3/LuaSnip",
+    -- Handle jumping between nodes below.
     keys = function()
       return {}
     end,
   },
-  -- then: setup supertab in cmp
+
   {
     "hrsh7th/nvim-cmp",
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0
-          and vim.api
-              .nvim_buf_get_lines(0, line - 1, line, true)[1]
-              :sub(col, col)
-              :match("%s")
-            == nil
-      end
-
-      local luasnip = require("luasnip")
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
+      local fn = {
+        -- Accept the currently suggested completion item, replacing the current
+        -- word
+        completeSuggested = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true, -- As shown on ghost_text
+        }),
+
+        -- Accept the currently selected completion item, replacing the current
+        -- word.
+        completeSelected = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false, -- No complete if not explicitly selected
+        }),
+
+        -- Move cursor to next item in the completion menu. If completion menu
+        -- is not showing, we jump to the next snippet node *only* if we are
+        -- inside a snippet. This stops us from jumping back to an unexpected
+        -- place.
+        nextItem = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- they way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
+          elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
           else
             fallback()
           end
         end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+
+        -- Move cursor to previous item in the completion menu. If completion
+        -- menu is not showing, we jump to the next snippet node *only* if we
+        -- are inside a snippet. This stops us from jumping back to an
+        -- unexpected place.
+        prevItem = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
+          elseif luasnip.locally_jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
           end
         end, { "i", "s" }),
+
+        -- Closes the completion menu if it is showing, then executes the
+        -- keypress.
+        unobstrusive = function(fallback)
+          if cmp.visible() then
+            cmp.mapping.abort()
+          end
+          fallback()
+        end,
+      }
+
+      opts.completion = {
+        completeopt = "menu,menuone,noselect,preview",
+      }
+
+      opts.mapping = cmp.mapping.preset.insert({
+        -- Scroll through docs in preview window.
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+        -- Pop up completion menu if not already showing
+        ["<C-Space>"] = cmp.mapping.complete(),
+
+        -- Close completion menu.
+        ["<Esc>"] = cmp.mapping.abort(),
+
+        -- Accept currently selected item. Will not auto-select an item.
+        ["<CR>"] = fn.completeSelected,
+
+        -- Select next/previous item.
+        ["<Tab>"] = fn.nextItem,
+        ["<S-Tab>"] = fn.prevItem,
       })
     end,
   },
