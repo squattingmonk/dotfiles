0ed759388e749708da31690f97a1fd042c9313e8
Michael A. Sinclair <squattingmonk@gmail.com>
2023-03-31 15:47:22 -0500

    neovim: customize colors

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -12,6 +12,20 @@ return {
       vim.g.nord_italic = false
       vim.g.nord_bold = false
       vim.g.nord_borders = true
+
+      local hl = vim.api.nvim_set_hl
+      local group = vim.api.nvim_create_augroup("NordFix", { clear = true })
+      local colors = require("nord.named_colors")
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        desc = "Customize colors in Nord colorscheme",
+        pattern = "nord",
+        group = group,
+        callback = function()
+          hl(0, "LspCodeLens", { link = "Comment" })
+          hl(0, "Special", { fg = colors.teal })
+        end,
+      })
     end,
   },
 }
