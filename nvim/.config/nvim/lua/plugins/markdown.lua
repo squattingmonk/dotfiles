8af5edcab767f5cb6b8f9f306ae167df497de1d8
Michael A. Sinclair <squattingmonk@gmail.com>
2024-10-30 11:05:42 -0500

    neovim: add markdown config

---
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,21 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    code = {
+      sign = true,
+      width = "block",
+      min_width = 80,
+      right_pad = 1,
+    },
+    heading = {
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+      signs = { "󰫎 " },
+      width = "block",
+      min_width = 80,
+    },
+    dash = {
+      icon = "─",
+      width = 80,
+    },
+  },
+}
