7e45db14de4afaf90a6710c8607511fb00a79450
Michael A. Sinclair <squattingmonk@gmail.com>
2025-01-31 11:05:48 -0600

    neovim: fix luasnip jumping

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,16 +1,33 @@
 return {
   {
     "Saghen/blink.cmp",
-    opts = {
-      completion = {
+    opts = function(_, opts)
+      -- Merge custom sources with the existing ones from LazyVim
+      -- Requires the LazyVim blink.cmp extra
+      opts.snippets = {
+        preset = "luasnip",
+        expand = function(snippet)
+          require("luasnip").lsp_expand(snippet)
+        end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require("luasnip").jumpable(filter.direction)
+          end
+          return require("luasnip").in_snippet()
+        end,
+        jump = function(direction)
+          require("luasnip").jump(direction)
+        end,
+      }
+      opts.completion = {
         -- Don't select by default, auto insert on selection
         list = { selection = { preselect = false, auto_insert = true } },
-      },
-      keymap = {
+      }
+      opts.keymap = {
         preset = "enter",
         ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-      },
-    },
+      }
+    end,
   },
 }
