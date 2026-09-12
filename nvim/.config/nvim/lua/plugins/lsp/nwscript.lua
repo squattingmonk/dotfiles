24a6304f8b408f6f423f251e67bb8dc51dfdbe68
Michael A. Sinclair <squattingmonk@gmail.com>
2026-09-11 23:23:44 -0500

    nvim: formatting fix for nwscript lsp

--- nvim/.config/nvim/lua/plugins/lsp/nwscript.lua
+++ nvim/.config/nvim/lua/plugins/lsp/nwscript.lua
@@ -1,23 +1,21 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        nwscript = {
-          settings = {
-            ["nwscript-ee-lsp"] = {
-              autoCompleteFunctionsWithParams = false,
-              includeCommentsInFunctionsHover = true,
-              formatter = {
-                enabled = false,
-              },
-              compiler = {
-                enabled = true,
-                verbose = true,
-                nwnHome = os.getenv("XDG_DATA_HOME") .. "/Neverwinter Nights",
-                nwnInstallation = os.getenv("STEAM_PATH")
-                  .. "/Neverwinter Nights",
-              },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      nwscript = {
+        settings = {
+          ["nwscript-ee-lsp"] = {
+            autoCompleteFunctionsWithParams = false,
+            includeCommentsInFunctionsHover = true,
+            formatter = {
+              enabled = false,
+            },
+            compiler = {
+              enabled = true,
+              verbose = true,
+              nwnHome = os.getenv("XDG_DATA_HOME") .. "/Neverwinter Nights",
+              nwnInstallation = os.getenv("STEAM_PATH")
+                .. "/Neverwinter Nights",
             },
           },
         },
